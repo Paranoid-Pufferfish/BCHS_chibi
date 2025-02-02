81279559fd3f5614cf5104ee226350cc309c6433
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-02-02 01:28:00 +0100

    going back

--- main.c
+++ main.c
@@ -1,39 +1,103 @@
-/* Title: Database example */
+#include <sys/types.h> /* size_t, ssize_t */
+#include <stdarg.h> /* va_list */
+#include <stdint.h> /* int64_t */
+#include <unistd.h> /* pledge() */
+#include <err.h> /* err(), warnx() */
+#include <stdlib.h> /* EXIT_FAILURE */
+#include <string.h> /* memset() */
+#include <kcgi.h>
+#include <kcgihtml.h>
+#include <sqlbox.h>
 
-#include <inttypes.h>
-#include <stdlib.h>
-#include <stdio.h>
 
-#include <ksql.h>
+int main(void) {
+    struct kreq r;
+    struct khtmlreq req;
+    struct sqlbox *p;
+    struct sqlbox_cfg cfg;
+    size_t id, stmtid;
+    struct sqlbox_src srcs[] = {
+        {
+            .fname = (char *) "db.db",
+            .mode = SQLBOX_SRC_RW
+        }
+    };
+    struct sqlbox_pstmt stmts[] = {
+        {
+            .stmt = (char *) "INSERT INTO BCHS(NUM, STR) VALUES (?,?)"
+        }
+    };
+    struct sqlbox_parm parms[] = {
+        {
+            .iparm = 99,
+            .type = SQLBOX_PARM_INT
+        },
+        {
+            .sparm = "It worked Fuckers!!!\0",
+            .type = SQLBOX_PARM_STRING,
+            .sz = 0
 
-int
- main(void)
- {
- 	struct ksql	*sql;
- 	struct ksqlstmt	*stmt;
- 	int64_t		 hits;
+        }
+    };
+    if (pledge("stdio rpath cpath wpath "
+               "flock proc fattr", NULL) == -1)
+        err(EXIT_FAILURE, "pledge");
 
- 	/* First, initialise, open, read, then close the db. */
+    memset(&cfg, 0, sizeof(struct sqlbox_cfg));
+    cfg.msg.func_short = warnx;
+    cfg.srcs.srcs = srcs;
+    cfg.srcs.srcsz = 1;
+    cfg.stmts.stmts = stmts;
+    cfg.stmts.stmtsz = 1;
+    if ((p = sqlbox_alloc(&cfg)) == NULL)
+        errx(EXIT_FAILURE, "sqlbox_alloc");
+    // if (pledge("stdio proc", NULL) == -1)
+    //     err(EXIT_FAILURE, "pledge");
+    if (!(id = sqlbox_open(p, 0)))
+        errx(EXIT_FAILURE, "sqlbox_open");
+    // if (!(stmtid = sqlbox_prepare_bind(p, id, 0, 2, parms, 0)))
+    //     errx(EXIT_FAILURE, "sqlbox_prepare_bind");
+    //
+    // const struct sqlbox_parmset *res = sqlbox_step(p, stmtid);
+    if (!sqlbox_exec(p,id,0,2,parms,0))
+        errx(EXIT_FAILURE, "sqlbox_exec");
+    /*if (!(sqlbox_finalise(p, stmtid)))
+        errx(EXIT_FAILURE, "sqlbox_finalise");*/
+    sqlbox_free(p);
+    if (khttp_parse(&r, 0, 0, 0, 0, 0) != KCGI_OK)
+        return 1;
+    // if (pledge("stdio", NULL) == -1)
+    //     err(EXIT_FAILURE, "pledge");
+    khttp_head(&r, kresps[KRESP_STATUS],
+               "%s", khttps[KHTTP_200]);
+    khttp_head(&r, kresps[KRESP_CONTENT_TYPE],
+               "%s", kmimetypes[r.mime]);
+    khttp_body(&r);
+    khtml_open(&req, &r, 0);
+    kcgi_writer_disable(&r);
+    khtml_printf(&req, "path: %s", r.path);
+    khtml_elem(&req, KELEM_BR);
+    khtml_printf(&req, "suffix: %s", r.suffix);
+    khtml_elem(&req, KELEM_BR);
+    khtml_printf(&req, "fullpath: %s", r.fullpath);
+    khtml_elem(&req, KELEM_BR);
+    khtml_printf(&req, "pagename: %s", r.pagename);
+    khtml_elem(&req, KELEM_BR);
+    khtml_printf(&req, "host: %s", r.host);
+    khtml_elem(&req, KELEM_BR);
+    khtml_printf(&req, "port: %d", r.port);
+    khtml_elem(&req, KELEM_BR);
+    khtml_printf(&req, "mime: %s", kmimetypes[r.mime]);
+    khtml_elem(&req, KELEM_BR);
+    struct khead *h;
+    for (int i = 0; i < KREQU__MAX; i++) {
+        if ((h = r.reqmap[i]) != NULL) {
+            khtml_printf(&req, "%s: %s", h->key, h->val);
+            khtml_elem(&req, KELEM_BR);
+        }
+    }
+    khtml_close(&req);
+    khttp_free(&r);
 
- 	sql = ksql_alloc(NULL);
- 	ksql_open(sql, "db.db");
- 	ksql_stmt_alloc(sql, &stmt,
- 		"INSERT INTO BCHS(NUM, STR) VALUES (99,'Hello Suckers!!!')", 0);
- 	ksql_stmt_step(stmt);
- 	ksql_stmt_free(stmt);
- 	ksql_stmt_alloc(sql, &stmt,
- 		"SELECT NUM FROM BCHS", 0);
- 	ksql_stmt_step(stmt);
- 	hits = ksql_stmt_int(stmt, 0);
- 	ksql_stmt_free(stmt);
- 	ksql_free(sql);
-
- 	/* Now print the result. */
-
- 	puts("Status: 200 OK\r");
- 	puts("Content-Type: text/html\r");
- 	puts("\r");
- 	printf("Hello, world: %" PRId64 "\n", hits);
-
- 	return(EXIT_SUCCESS);
- }
+    return 0;
+}
