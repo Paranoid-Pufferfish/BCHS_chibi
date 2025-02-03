17b1c0ec8fce229c00edd71f37dc5cf5e54f0e63
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-02-03 20:26:46 +0100

    Try pledging

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <err.h> /* err(), warnx() */
 #include <stdlib.h> /* EXIT_FAILURE */
 #include <string.h> /* memset() */
+#include <kcgi.h>
+#include <kcgihtml.h>
 #include <sqlbox.h>
 
 
@@ -12,6 +14,8 @@ int main(void) {
     size_t dbid, stmtid;
     struct sqlbox *p;
     struct sqlbox_cfg cfg;
+    struct kreq r;
+    struct khtmlreq req;
     struct sqlbox_src srcs[] = {
         { .fname = (char *)"db.db",
           .mode = SQLBOX_SRC_RW }
@@ -32,17 +36,60 @@ int main(void) {
     cfg.stmts.stmtsz = 1;
     cfg.stmts.stmts = pstmts;
 
+    if (pledge("stdio rpath cpath "
+        "wpath flock proc fattr", NULL) == -1)
+        err(EXIT_FAILURE, "pledge");
     if ((p = sqlbox_alloc(&cfg)) == NULL)
         errx(EXIT_FAILURE, "sqlbox_alloc");
+
+    if (pledge("stdio proc", NULL) == -1)
+        err(EXIT_FAILURE, "pledge");
     if (!(dbid = sqlbox_open(p, 0)))
         errx(EXIT_FAILURE, "sqlbox_open");
+    if (pledge("stdio", NULL) == -1)
+        err(EXIT_FAILURE, "pledge");
     if (!(stmtid = sqlbox_prepare_bind(p, dbid, 0, 1, parms, 0)))
         errx(EXIT_FAILURE, "sqlbox_prepare_bind");
     if ((res = sqlbox_step(p, stmtid)) == NULL)
         errx(EXIT_FAILURE, "sqlbox_step");
     if (!sqlbox_finalise(p, stmtid))
         errx(EXIT_FAILURE, "sqlbox_finalise");
-
     sqlbox_free(p);
+    if (pledge("stdio proc", NULL) == -1)
+        err(EXIT_FAILURE, "pledge");
+    if (khttp_parse(&r, 0, 0, 0, 0, 0) != KCGI_OK)
+        return 1;
+    if (pledge("stdio", NULL) == -1)
+        err(EXIT_FAILURE, "pledge");
+    khttp_head(&r, kresps[KRESP_STATUS],
+               "%s", khttps[KHTTP_200]);
+    khttp_head(&r, kresps[KRESP_CONTENT_TYPE],
+               "%s", kmimetypes[r.mime]);
+    khttp_body(&r);
+    khtml_open(&req, &r, 0);
+    kcgi_writer_disable(&r);
+    khtml_printf(&req, "path: %s", r.path);
+    khtml_elem(&req, KELEM_BR);
+    khtml_printf(&req, "suffix: %s", r.suffix);
+    khtml_elem(&req, KELEM_BR);
+    khtml_printf(&req, "fullpath: %s", r.fullpath);
+    khtml_elem(&req, KELEM_BR);
+    khtml_printf(&req, "pagename: %s", r.pagename);
+    khtml_elem(&req, KELEM_BR);
+    khtml_printf(&req, "host: %s", r.host);
+    khtml_elem(&req, KELEM_BR);
+    khtml_printf(&req, "port: %d", r.port);
+    khtml_elem(&req, KELEM_BR);
+    khtml_printf(&req, "mime: %s", kmimetypes[r.mime]);
+    khtml_elem(&req, KELEM_BR);
+    struct khead *h;
+    for (int i = 0; i < KREQU__MAX; i++) {
+        if ((h = r.reqmap[i]) != NULL) {
+            khtml_printf(&req, "%s: %s", h->key, h->val);
+            khtml_elem(&req, KELEM_BR);
+        }
+    }
+    khtml_close(&req);
+    khttp_free(&r);
     return 0;
 }
