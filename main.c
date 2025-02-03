78282a74ee574bcb829be19ac9276a80c09c97ff
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-02-03 21:57:01 +0100

    small changes

--- main.c
+++ main.c
@@ -11,6 +11,31 @@
 #include <sqlbox.h>
 
 
+enum key {
+    KEY_VAL,
+    KEY__MAX
+};
+
+
+static const struct kvalid keys[KEY__MAX] = {
+    {kvalid_int, "val"}, /* KEY_INTEGER */
+};
+
+
+enum page {
+    PAGE_INDEX,
+    PAGE_ADD,
+    PAGE_GET,
+    PAGE__MAX
+};
+
+const char *const pages[PAGE__MAX] = {
+    "index",
+    "add",
+    "get"
+};
+
+
 int main(void) {
     struct kreq r;
     struct khtmlreq req;
@@ -54,41 +79,26 @@ int main(void) {
     // if (!sqlbox_finalise(p, stmtid))
     //     errx(EXIT_FAILURE, "sqlbox_finalise");
     // sqlbox_free(p);
-
+    enum khttp er = KHTTP_200;
     if (pledge("stdio proc", NULL) == -1)
         err(EXIT_FAILURE, "pledge");
-    if (khttp_parse(&r, 0, 0, 0, 0, 0) != KCGI_OK)
+    if (khttp_parse(&r, keys, KEY__MAX, pages, PAGE__MAX, PAGE_INDEX) != KCGI_OK)
         return 1;
     if (pledge("stdio", NULL) == -1)
         err(EXIT_FAILURE, "pledge");
+    if (r.mime != KMIME_TEXT_HTML)
+        er = KHTTP_404;
+    if (r.method != KMETHOD_GET)
+        er = KHTTP_405;
     khttp_head(&r, kresps[KRESP_STATUS],
-               "%s", khttps[KHTTP_200]);
+               "%s", khttps[er]);
     khttp_head(&r, kresps[KRESP_CONTENT_TYPE],
-               "%s", kmimetypes[r.mime]);
+               "%s", kmimetypes[KMIME_TEXT_HTML]);
     khttp_body(&r);
     khtml_open(&req, &r, 0);
     kcgi_writer_disable(&r);
-    khtml_printf(&req, "path: %s", r.path);
-    khtml_elem(&req, KELEM_BR);
-    khtml_printf(&req, "suffix: %s", r.suffix);
-    khtml_elem(&req, KELEM_BR);
-    khtml_printf(&req, "fullpath: %s", r.fullpath);
-    khtml_elem(&req, KELEM_BR);
-    khtml_printf(&req, "pagename: %s", r.pagename);
-    khtml_elem(&req, KELEM_BR);
-    khtml_printf(&req, "host: %s", r.host);
-    khtml_elem(&req, KELEM_BR);
-    khtml_printf(&req, "port: %d", r.port);
-    khtml_elem(&req, KELEM_BR);
-    khtml_printf(&req, "mime: %s", kmimetypes[r.mime]);
-    khtml_elem(&req, KELEM_BR);
-    struct khead *h;
-    for (int i = 0; i < KREQU__MAX; i++) {
-        if ((h = r.reqmap[i]) != NULL) {
-            khtml_printf(&req, "%s: %s", h->key, h->val);
-            khtml_elem(&req, KELEM_BR);
-        }
-    }
+    khtml_elem(&req,KELEM_H1);
+    khtml_printf(&req,"Hello World!");
     khtml_close(&req);
     khttp_free(&r);
     return 0;
