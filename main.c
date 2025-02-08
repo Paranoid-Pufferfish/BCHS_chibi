554c10896ed68b9fb332d847ec86b330b75c0428
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-02-08 23:02:14 +0100

    Third

--- main.c
+++ main.c
@@ -50,6 +50,8 @@ int main(void) {
     if ((res = sqlbox_step(p2, stmtid)) == NULL)
         errx(EXIT_FAILURE, "sqlbox_step");
     if ((res = sqlbox_step(p2, stmtid)) == NULL)
+        errx(EXIT_FAILURE, "sqlbox_step");
+    if ((res = sqlbox_step(p2, stmtid)) == NULL)
         errx(EXIT_FAILURE, "Third sqlbox_step");
 
     enum khttp er = KHTTP_200;
