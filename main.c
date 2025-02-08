b7c0b6c1e87793a429cce4cdcada152d9a75554f
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-02-08 22:18:45 +0100

    Test

--- main.c
+++ main.c
@@ -47,12 +47,14 @@ int main(void) {
     struct sqlbox *p2;
     struct sqlbox_cfg cfg;
     struct sqlbox_src srcs[] = {
-        { .fname = (char *)"db/db.db",
-          .mode = SQLBOX_SRC_RWC }
+        {
+            .fname = (char *) "db/db.db",
+            .mode = SQLBOX_SRC_RWC
+        }
     };
     struct sqlbox_pstmt pstmts[] = {
-        { .stmt = (char *)"SELECT * FROM chibis"},
-      };
+        {.stmt = (char *) "SELECT * FROM chibis"},
+    };
     const struct sqlbox_parmset *res;
 
     memset(&cfg, 0, sizeof(struct sqlbox_cfg));
@@ -63,7 +65,7 @@ int main(void) {
     cfg.stmts.stmts = pstmts;
 
     if (pledge("stdio rpath cpath "
-        "wpath flock proc fattr", NULL) == -1)
+               "wpath flock proc fattr", NULL) == -1)
         err(EXIT_FAILURE, "pledge");
     if ((p2 = sqlbox_alloc(&cfg)) == NULL)
         errx(EXIT_FAILURE, "sqlbox_alloc");
@@ -75,8 +77,6 @@ int main(void) {
         errx(EXIT_FAILURE, "sqlbox_prepare_bind");
     if ((res = sqlbox_step(p2, stmtid)) == NULL)
         errx(EXIT_FAILURE, "sqlbox_step");
-
-
 
     enum khttp er = KHTTP_200;
     if (pledge("stdio proc", NULL) == -1)
@@ -103,18 +103,36 @@ int main(void) {
             khtml_printf(&req, "You are in ADD");
             break;
         case PAGE_GET: khtml_printf(&req, "You are in GET");
-            khtml_elem(&req, KELEM_BR);
-            if ((p = r.fieldmap[KEY_CCID]))
-                khtml_printf(&req, "CCID: %s", p->parsed.s);
-            khtml_elem(&req, KELEM_BR);
-             if ((p = r.fieldmap[KEY_CCINDEX]))
-                khtml_printf(&req, "CCINDEX: %lld", p->parsed.i);
+            for (int i = 0; i < res->psz; ++i) {
+                switch (res->ps[i].type) {
+                    case SQLBOX_PARM_BLOB:
+                        khtml_printf(&req,"Blob: %zu bytes\n", res->ps[i].sz);
+                        khtml_elem(&req,KELEM_BR);
+                        break;
+                    case SQLBOX_PARM_FLOAT:
+                        khtml_printf(&req,"Float: %f\n", res->ps[i].fparm);
+                        khtml_elem(&req,KELEM_BR);
+                        break;
+                    case SQLBOX_PARM_INT:
+                        khtml_printf(&req,"Blob: %lld\n", res->ps[i].iparm);
+                        khtml_elem(&req,KELEM_BR);
+                        break;
+                    case SQLBOX_PARM_NULL:
+                        khtml_printf(&req,"Null\n");
+                        khtml_elem(&req,KELEM_BR);
+                        break;
+                    case SQLBOX_PARM_STRING:
+                        khtml_printf(&req,"String: %s\n", res->ps[i].sparm);
+                        khtml_elem(&req,KELEM_BR);
+                        break;
+                }
+            }
             break;
         default: khtml_printf(&req, "Probably Elsewhere");
             break;
     }
 
-     if (!sqlbox_finalise(p2, stmtid))
+    if (!sqlbox_finalise(p2, stmtid))
         errx(EXIT_FAILURE, "sqlbox_finalise");
     sqlbox_free(p2);
     khtml_close(&req);
