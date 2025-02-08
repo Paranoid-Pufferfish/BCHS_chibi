328bd978e99d35a25d8c2315fd92b1ac055bdbe9
Paranoid-Pufferfish <fouadeddine.aitmeddour@se.univ-bejaia.dz>
2025-02-08 22:09:00 +0100

    Forgot what i Did

--- main.c
+++ main.c
@@ -43,45 +43,41 @@ int main(void) {
     struct kpair *p;
     struct khtmlreq req;
 
-    // size_t dbid, stmtid;
-    // struct sqlbox *p;
-    // struct sqlbox_cfg cfg;
-    // struct sqlbox_src srcs[] = {
-    //     { .fname = (char *)"db/db.db",
-    //       .mode = SQLBOX_SRC_RWC }
-    // };
-    // struct sqlbox_pstmt pstmts[] = {
-    //     { .stmt = (char *)"INSERT INTO BCHS (NUM) VALUES (?)" },
-    //   };
-    // struct sqlbox_parm parms[] = {
-    //     { .iparm = 10,
-    //       .type = SQLBOX_PARM_INT },
-    //   };
-    // const struct sqlbox_parmset *res;
-    //
-    // memset(&cfg, 0, sizeof(struct sqlbox_cfg));
-    // cfg.msg.func_short = warnx;
-    // cfg.srcs.srcsz = 1;
-    // cfg.srcs.srcs = srcs;
-    // cfg.stmts.stmtsz = 1;
-    // cfg.stmts.stmts = pstmts;
-    //
-    // if (pledge("stdio rpath cpath "
-    //     "wpath flock proc fattr", NULL) == -1)
-    //     err(EXIT_FAILURE, "pledge");
-    // if ((p = sqlbox_alloc(&cfg)) == NULL)
-    //     errx(EXIT_FAILURE, "sqlbox_alloc");
-    // if (pledge("stdio proc", NULL) == -1)
-    //     err(EXIT_FAILURE, "pledge");
-    // if (!(dbid = sqlbox_open(p, 0)))
-    //     errx(EXIT_FAILURE, "sqlbox_open");
-    // if (!(stmtid = sqlbox_prepare_bind(p, dbid, 0, 1, parms, 0)))
-    //     errx(EXIT_FAILURE, "sqlbox_prepare_bind");
-    // if ((res = sqlbox_step(p, stmtid)) == NULL)
-    //     errx(EXIT_FAILURE, "sqlbox_step");
-    // if (!sqlbox_finalise(p, stmtid))
-    //     errx(EXIT_FAILURE, "sqlbox_finalise");
-    // sqlbox_free(p);
+    size_t dbid, stmtid;
+    struct sqlbox *p2;
+    struct sqlbox_cfg cfg;
+    struct sqlbox_src srcs[] = {
+        { .fname = (char *)"db/db.db",
+          .mode = SQLBOX_SRC_RWC }
+    };
+    struct sqlbox_pstmt pstmts[] = {
+        { .stmt = (char *)"SELECT (CCID,CCINDEX) FROM chibis"},
+      };
+    const struct sqlbox_parmset *res;
+
+    memset(&cfg, 0, sizeof(struct sqlbox_cfg));
+    cfg.msg.func_short = warnx;
+    cfg.srcs.srcsz = 1;
+    cfg.srcs.srcs = srcs;
+    cfg.stmts.stmtsz = 1;
+    cfg.stmts.stmts = pstmts;
+
+    if (pledge("stdio rpath cpath "
+        "wpath flock proc fattr", NULL) == -1)
+        err(EXIT_FAILURE, "pledge");
+    if ((p2 = sqlbox_alloc(&cfg)) == NULL)
+        errx(EXIT_FAILURE, "sqlbox_alloc");
+    if (pledge("stdio proc", NULL) == -1)
+        err(EXIT_FAILURE, "pledge");
+    if (!(dbid = sqlbox_open(p2, 0)))
+        errx(EXIT_FAILURE, "sqlbox_open");
+    if (!(stmtid = sqlbox_prepare_bind(p2, dbid, 0, 0, 0, 0)))
+        errx(EXIT_FAILURE, "sqlbox_prepare_bind");
+    if ((res = sqlbox_step(p2, stmtid)) == NULL)
+        errx(EXIT_FAILURE, "sqlbox_step");
+
+
+
     enum khttp er = KHTTP_200;
     if (pledge("stdio proc", NULL) == -1)
         err(EXIT_FAILURE, "pledge");
@@ -100,8 +96,6 @@ int main(void) {
     khttp_body(&r);
     khtml_open(&req, &r, 0);
     kcgi_writer_disable(&r);
-    khtml_elem(&req, KELEM_H1);
-
     switch (r.page) {
         case PAGE_INDEX: khtml_printf(&req, "You are in INDEX");
             break;
@@ -119,7 +113,10 @@ int main(void) {
         default: khtml_printf(&req, "Probably Elsewhere");
             break;
     }
-    khtml_closeelem(&req, 0);
+
+     if (!sqlbox_finalise(p2, stmtid))
+        errx(EXIT_FAILURE, "sqlbox_finalise");
+    sqlbox_free(p2);
     khtml_close(&req);
     khttp_free(&r);
     return 0;
